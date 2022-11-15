7f599b9bb2f528193bfa11b76f23e466272f141a
LudwigEffort <dr.feynman65@gmail.com>
2022-11-15 22:59:00 +0100

    add data

--- js/script.js
+++ js/script.js
@@ -15,3 +15,15 @@ Bonus:
 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
 */
+
+new Vue({
+    el: '#root',
+    data: {
+        todos: [
+            'Do homeworks',
+            'Do shopping',
+            'Make dinner'
+        ],
+    }
+}
+);
